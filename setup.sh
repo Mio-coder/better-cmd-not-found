3ec6e7bff9ae216bdd02cdcc3bc89f01b7e7f380
Mio-coder <78543744+Mio-coder@users.noreply.github.com>
2025-05-21 21:25:50 +0200

    fix 4

--- setup.sh
+++ setup.sh
@@ -1,15 +1,12 @@
 # shellcheck shell=sh
 
-if [ -n "$BASH_VERSION" ]; then
-    command_not_found_handle() {
-        # shellcheck disable=SC2317
-        @out@/bin/better-cmd-not-found "$@"
-        return $?
-    }
-elif [ -n "$ZSH_VERSION" ]; then
-    command_not_found_handler() {
-        # shellcheck disable=SC2317
-        @out@/bin/better-cmd-not-found "$@"
-        return $?
-    }
-fi
+command_not_found_handle() {
+    # shellcheck disable=SC2317
+    @out@/bin/better-cmd-not-found "$@"
+    return $?
+}
+command_not_found_handler() {
+    # shellcheck disable=SC2317
+    @out@/bin/better-cmd-not-found "$@"
+    return $?
+}
